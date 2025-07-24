a699b018e0696dc2ef24e874f4845657460b13d0
Bakry Grouo <bakrygrouo@gmail.com>
2025-07-24 11:51:08 +0000

    apakah projek kita sudah ada fitur PWA, aku sudah hubungkan ke vercel. a

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+
 import type {Metadata} from 'next';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster";
@@ -6,6 +7,7 @@ import { SidebarProvider } from '@/components/ui/sidebar';
 export const metadata: Metadata = {
   title: 'Rajamania - Situs Slot Online Gacor dan Agen Bandar Bola Terpercaya',
   description: 'Rajamania merupakan salah satu situs judi slot online gacor dengan sistem taruhan paling aman dan agen bandar bola terpercaya.',
+  manifest: '/manifest.json',
 };
 
 export default function RootLayout({
@@ -19,6 +21,8 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Avenir:wght@400;500;700;900&display=swap" rel="stylesheet" />
+        <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
+        <meta name="theme-color" content="#181E26" />
       </head>
       <body className="font-body antialiased">
         <SidebarProvider>
